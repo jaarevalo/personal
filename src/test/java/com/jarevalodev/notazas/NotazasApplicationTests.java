5603d1e8e23faef678961c07155e28a611adafaf
Jesús Arévalo <jaarevalo@paradigmadigital.com>
2021-07-30 20:16:28 +0200

    Adding a dummy test

--- src/test/java/com/jarevalodev/notazas/NotazasApplicationTests.java
+++ src/test/java/com/jarevalodev/notazas/NotazasApplicationTests.java
@@ -3,11 +3,19 @@ package com.jarevalodev.notazas;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
 @SpringBootTest
 class NotazasApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void should_do_stuff(){
+		System.out.println("hi");
+		assertTrue("This will succeed.", true);
 	}
 
 }
